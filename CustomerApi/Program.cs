67f0b659f4d847c501f7debd0810d5518f2f01a6
RocknRule <coldstar.band@gmail.com>
2022-06-10 22:49:32 -0400

    Fixed bin

--- CustomerApi/Program.cs
+++ CustomerApi/Program.cs
@@ -1,16 +1,11 @@
-using CustomerBL;
-using CustomerDL;
-using CustomerModel;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddScoped<IRepository<Customer>, SQLCustomerRepository>(repo => new SQLCustomerRepository(Environment.GetEnvironmentVariable("Connection_String")));
-builder.Services.AddScoped<ICustomerBL, CustomersBL>();
 
 var app = builder.Build();
 
